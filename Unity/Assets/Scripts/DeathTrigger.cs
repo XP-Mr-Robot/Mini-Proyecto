8d515bad64a7ab1b63b20f9bf758d39dccd13448
Leonardo Sanchez <leonsanchez09@gmail.com>
2020-07-02 04:00:15 -0500

    Se creo DeathTrigger y Prefab de enemigo

---
+++ Unity/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DeathTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
